6fd23641fee7a2f6ee3f7160762797cdaedef81a
Moritz Ott <87697757+moritzott@users.noreply.github.com>
2021-08-02 11:05:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -319,7 +319,7 @@ function registerServiceWorker(){
     // Make sure service worker are supported
     if ('serviceWorker' in navigator){
         navigator.serviceWorker
-            .register('/serviceworker.js')
+            .register('./serviceworker.js')
             .then(registrationObject => console.log('ServiceWorker registered.'))
             .catch(error => console.log(`ServiceWorker Error: ${error}`))
     } else {
